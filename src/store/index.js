99be8e7b428f89ed2b03516d461eee1d824622d7
Allen Mordukhaev <allenmor93@gmail.com>
2022-11-15 18:47:16 -0500

    split reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,53 +1,21 @@
-import { act } from '@testing-library/react'
+import { configureStore } from "@reduxjs/toolkit"
+import counterReducer from "./counter"
+import authReducer from "./auth"
 
-import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const initialCounterState = {counter: 0, showCounter: true}
 
 export const INCREMENT = 'increment'
 
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            state.counter ++
-        },
-        decrement(state) {
-            state.counter --
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter
-        }
-    }
-})
 
-const initialAuthState = {
-    isAuthenticated: false
-}
 
-const authSlice = createSlice({
-    name: 'auth',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true
-        },
-        logout(state) {
-            state.isAuthenticated = false
-        }
-    }
-})
+
 
 
 
 const store =  configureStore({
     reducer: {
-        counter: counterSlice.reducer,
-        auth: authSlice.reducer
+        counter: counterReducer,
+        auth: authReducer
     }
 })
 
